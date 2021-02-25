6acd680326b41bd643e90509392adaad8e93f010
Cesar Velásquez Cordova <ces.velasquez@hotmail.com>
2021-02-25 14:28:35 -0600

    Middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,30 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { CatsController } from './cats/controllers/cats.controller';
+import { logger, LoggerMiddleware } from './common/logger.middleware';
 
 @Module({
   imports: [CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(logger)
+      // .forRoutes('cats');
+      // .forRoutes({ path: 'cats', method: RequestMethod.GET });
+      .exclude({
+        path: 'cats',
+        method: RequestMethod.GET,
+      })
+      .forRoutes(CatsController);
+  }
+}
---
+++ src/common/logger.middleware.ts
@@ -0,0 +1,15 @@
+import { Injectable, NestMiddleware } from "@nestjs/common";
+import { NextFunction, Request, Response } from "express";
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+    use(req: Request, res: Response, next: NextFunction) {
+        console.log('Request...', req.method);
+        next();
+    }
+}
+
+export const logger = (req: Request, res: Response, next: NextFunction) => {
+    console.log('Functional middleware...', req.method);
+    next();
+}
